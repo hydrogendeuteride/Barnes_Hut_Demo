854e74f4fec1722063c43f04bffa3a90bd38e809
hydrogendeuteride <dumpsterowner@gmail.com>
2023-03-04 15:58:49 +0900

    distribution function add but center black hole is not working

--- main.cc
+++ main.cc
@@ -3,6 +3,7 @@
 #include <chrono>
 #include "barnes_hut.h"
 #include "Drawable.h"
+#include <cmath>
 
 void postorder(const std::shared_ptr<Node> &Node)
 {
@@ -17,30 +18,56 @@ void postorder(const std::shared_ptr<Node> &Node)
     std::cout << Node->width << " " << Node->height << " " << Node->data.size () << std::endl;
 }
 
-void DiskDistribution(std::vector<Body>& bodies, int numbe)
+void DiskDistribution(
+        std::vector<Body> &bodies, int number, double rad_max, double rad_min,
+        double mass_min, double mass_max, double centermass)
 {
+    std::random_device rand;
+    std::mt19937 gen (rand ());
+    std::uniform_real_distribution<> rand_angle (0, 2 * M_PI);
+    std::uniform_real_distribution<> rand_dist (rad_min, rad_max);
+    std::uniform_real_distribution<> rand_mass (mass_min, mass_max);
 
+    bodies.emplace_back(vec2(SimWidth / 2.0, SimHeight / 2.0), vec2(0.0, 0.0), centermass);
+
+    for (int i = 0; i < number; ++i)
+    {
+        double angle = rand_angle (gen);
+        double distance = rand_dist (gen);
+        double pos_x = std::cos (angle) * distance + (ViewWidth / 2.0);
+        double pos_y = std::sin (angle) * distance + (ViewHeight / 2.0);
+
+        double velocity = std::pow (centermass * GRAVITY_CONST / distance, 0.5);
+        double vel_x = std::sin (angle) * velocity;
+        double vel_y = -std::cos (angle) * velocity;
+
+        double mass = rand_mass (gen);
+
+        bodies.emplace_back (vec2 (pos_x, pos_y), vec2 (vel_x, vel_y), mass);
+    }
 }
 
 int main()
 {
-    std::random_device rand;
+    /*std::random_device rand;
     std::mt19937 gen (rand ());
     std::uniform_real_distribution<> dist (200.0, 800.0);
     std::uniform_real_distribution<> mass (5.0, 20.0);
-
+*/
     std::vector<Body> bodies;
-    for (int i = 0; i < 500; ++i)
-        bodies.emplace_back (vec2 (dist (gen), dist (gen)),
-                             vec2 (0.0, 0.0),
-                             mass (gen));
+    /*  for (int i = 0; i < 500; ++i)
+          bodies.emplace_back (vec2 (dist (gen), dist (gen)),
+                               vec2 (0.0, 0.0),
+                               mass (gen));
+  */
+    DiskDistribution (bodies, 1000, 20000, 50, 1, 2, 1000000);
 
     BarnesHutTree tree = BarnesHutTree ();
 
-    sf::RenderWindow window (sf::VideoMode (1920, 1080), "Barnes-Hut");
+    sf::RenderWindow window (sf::VideoMode (ViewWidth, ViewHeight), "Barnes-Hut");
     window.setVerticalSyncEnabled (true);
 
-    Event::View view(&window, 1000, 1000, 1920, 1080);
+    Event::View view (&window, SimWidth, SimHeight, ViewWidth, ViewHeight);
 
     Object::Particles particles (bodies);
 
@@ -55,20 +82,20 @@ int main()
                     window.close ();
             }
 
-            view.ViewTrsnsform(&window, &event);
+            view.ViewTrsnsform (&window, &event);
         }
         window.clear ();
 
         auto start = std::chrono::high_resolution_clock::now ();
 
-        tree.addNodeIterative (bodies, 1000, 1000);
+        tree.addNodeIterative (bodies, SimWidth, SimHeight);
 
         auto middle = std::chrono::high_resolution_clock::now ();
         auto treegen = std::chrono::duration_cast<std::chrono::milliseconds> (middle - start);
 
         for (auto &x: bodies)
         {
-            tree.CalcMovement (x, 0.01);
+            tree.CalcMovement (x, 10.0);
             tree.BoundaryDetection (x);
         }
 
