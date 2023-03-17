028e036475b5e028e9218ba5eca2fa857a3e44c3
hydrogendeuteride <dumpsterowner@gmail.com>
2023-03-17 18:48:14 +0900

    added Plummer distribution function

--- main.cc
+++ main.cc
@@ -23,13 +23,15 @@ void DiskDistribution(
         std::vector<Body> &bodies, int number, double rad_max, double rad_min,
         double mass_min, double mass_max, double centermass)
 {
+    //https://en.wikipedia.org/wiki/Plummer_model
+    //http://www.artcompsci.org/kali/vol/plummer/volume11.pdf
     std::random_device rand;
     std::mt19937 gen (rand ());
     std::uniform_real_distribution<> rand_angle (0, 2 * M_PI);
     std::uniform_real_distribution<> rand_dist (rad_min, rad_max);
     std::uniform_real_distribution<> rand_mass (mass_min, mass_max);
 
-    bodies.emplace_back(vec2(SimWidth / 2.0, SimHeight / 2.0), vec2(0.0, 0.0), centermass);
+    bodies.emplace_back (vec2 (SimWidth / 2.0, SimHeight / 2.0), vec2 (0.0, 0.0), centermass);
 
     for (int i = 1; i < number; ++i)
     {
@@ -48,11 +50,48 @@ void DiskDistribution(
     }
 }
 
+void PlummerDistribution(
+        std::vector<Body> &bodies, int number, double rad_max,
+        double x_center, double y_center)
+{
+    std::random_device rand;
+    std::mt19937 gen (rand ());
+    std::uniform_real_distribution<> phi_rand (0.0, 2 * M_PI);
+    std::uniform_real_distribution<> norm_rand (0.0, 1.0);
+
+    for (int i = 0; i < number; ++i)
+    {
+        double dist_norm = norm_rand (gen);
+        double phi = phi_rand (gen);
+
+        double r = rad_max / std::sqrt (std::pow (dist_norm, -0.66666666667) - 1.0);
+        vec2 pos = vec2 (r * std::cos (phi) * r + x_center, r * std::sin (phi) * r + y_center);
+
+        double x = 0.1;
+        double y;
+        while (1)
+        {
+            y = norm_rand (gen);
+            if (y < x * x * std::pow ((1 - x * x), 1.5))
+                break;
+            x = norm_rand (gen);
+        }
+
+        double v = x * std::sqrt (2.0) * std::pow((1 + r * r), -0.25);
+        double phi_v = 2.0 * M_PI * norm_rand(gen);
+
+        vec2 vel = v * vec2(std::cos(phi_v), std::sin(phi_v));
+
+        bodies.emplace_back (pos, vel, 1.0);
+    }
+}
+
 int main()
 {
     std::vector<Body> bodies;
 
-    DiskDistribution (bodies, 1000, 20000, 50, 1, 2, 1000000);
+    //DiskDistribution (bodies, 1000, 20000, 50, 1, 2, 1000000);
+    PlummerDistribution (bodies, 1000, 10, SimWidth / 2.0, SimHeight / 2.0);
 
     BarnesHutTree tree = BarnesHutTree ();
 
@@ -85,11 +124,11 @@ int main()
         auto middle = std::chrono::high_resolution_clock::now ();
         auto treegen = std::chrono::duration_cast<std::chrono::milliseconds> (middle - start);
 
-        #pragma omp parallel for num_threads(omp_get_max_threads())
-        for (size_t i = 0; i < bodies.size(); ++i)
+#pragma omp parallel for num_threads(omp_get_max_threads())
+        for (size_t i = 0; i < bodies.size (); ++i)
         {
-            tree.CalcMovement (bodies.at(i), 1.0);
-            tree.BoundaryDetection (bodies.at(i));
+            tree.CalcMovement (bodies.at (i), 1.0);
+            tree.BoundaryDetection (bodies.at (i));
         }
 
         auto stop = std::chrono::high_resolution_clock::now ();
